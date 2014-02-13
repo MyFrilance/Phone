b9e03a7690b21d27e806a10b9147d80ed4cca7e0
boolean1515 <boolean1515@gmail.com>
2014-02-14 00:03:12 +0600

    поехали

---
+++ Phone/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phone.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Result(string name, string surname)
+        {
+            var db = new PhoneBaseEntities1();
+            var ab = db.Abonenty.Where(n => n.Name == name && n.Surname == surname);
+
+            return Json(ab);
+        }
+
+    }
+}
---
+++ Phone/Scripts/ApplicationStart.js
@@ -0,0 +1,52 @@
+﻿var ViewModel = {
+    Name: ko.observable(''),
+    Surname: ko.observable(''),
+    PhoneNumber: ko.observable(''),
+};
+
+$(function () {
+
+    ko.applyBindings(ViewModel);
+
+    $("#b").click(buttonGo);
+    $("#acc").hide();
+});
+var check = 0;
+buttonGo = function () {
+    var errors = 0;
+    if ($("#inputName").val() == "") {
+        $("#inputName").val("Введите имя");
+        errors++;
+    }
+    if ($("#inputSurname").val() == "") {
+        $("#inputSurname").val("Введите фамилию");
+        errors++;
+    }
+    if(errors==0)
+        $.ajax({
+            url: "/Home/Result?name=" + $('#inputName').val() + "&surname=" + $('#inputSurname').val(),
+            type: "POST",
+            cache: false,
+            success: function (html) {
+                var options = {};
+
+                if (html.length == 0) {
+                    if (check == 0)
+                        $("#acc").toggle('blind', options, 500);
+
+                    ViewModel.PhoneNumber("Нет такого абонента");
+                    check++;
+
+                } else {
+                    if (check == 0)
+                        $("#acc").toggle('blind', options, 500);
+
+                    ViewModel.PhoneNumber(html[0].PhoneNumber);
+                    check++;
+                }
+            },
+
+
+        });
+
+};
